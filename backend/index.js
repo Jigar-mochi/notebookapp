c9bf729d7f4f84f91240ce9d530dc2055d8c3c37
Jigar-mochi <jigar102024@gmail.com>
2022-07-06 17:44:43 +0530

    learned mongodb some more

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ connectmongo();
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello jigarbhai')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello jigarbhai')
+// })
+app.use(express.json())
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
---
+++ backend/models/Notes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const NotesSchema = new Schema({
+    title: {
+        type: string,
+        required: true
+    },
+    description: {
+        type: string,
+        required: true,
+    },
+    tag: {
+        type: string
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('notes', NotesSchema)
